53b0a3e3e97f7a0776d4551399afe4e5661d151f
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-01-10 10:05:37 +0530

    Average problem done

---
+++ Day01-JavaProgrammingElements/Level1 practiceProblem/Average.java
@@ -0,0 +1,25 @@
+//create class average to calculate the sam's average persent mark in PCM
+public class Average{
+	
+    public static void main(String [] args){
+
+    //create a variable maths of type integer which store the mark of maths
+	int maths = 94;
+
+	//create a variable physics of type integer which store the mark of physics
+	int physics = 95;
+
+	//create a variable chemistry of type integer which store the chemistry of maths
+	int chemistry = 96);
+	
+	//here calculate the aveage mark of sam
+	double average = (double)((maths+physics+chemistry)/3);
+	
+	//here print sam's average mark
+	System.out.println(" Sam’s average mark in PCM is " + average);
+	}
+}
+	
+	
+	
+	 
