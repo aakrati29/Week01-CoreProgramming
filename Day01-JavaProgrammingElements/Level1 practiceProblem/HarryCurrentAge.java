e7179b7e44f92a779875726e48e370ce3bd493b7
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-01-10 10:01:35 +0530

    Harry's age problem done

---
+++ Day01-JavaProgrammingElements/Level1 practiceProblem/HarryCurrentAge.java
@@ -0,0 +1,16 @@
+//Create HarryCurrentAge class to calculate current age of harry 
+public class HarryCurrentAge{
+
+    public static void main(String [] args){
+	
+	//create a variable birthYear of type integer to store the harry's birth year
+	int birthYear = 2000;
+	
+	//create a variable currentYear of type integer to store the current year
+	int currentYear = 2024;
+	
+	//create a variable age to store the harry's current age
+	int currentAge = currentYear - birthYear;
+	System.out.println(" Harry's age in 2024 is " + CurrentAge);
+	}
+}
