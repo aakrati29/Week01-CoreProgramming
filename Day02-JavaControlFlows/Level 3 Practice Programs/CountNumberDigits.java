d695859cc761b017aaf5d0558db7d5e65abc0141
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-01-11 10:22:26 +0530

    Count number digits problem done

---
+++ Day02-JavaControlFlows/Level 3 Practice Programs/CountNumberDigits.java
@@ -0,0 +1,39 @@
+//here import all classes so that we can use their inbuilt functions
+import java.util.Scanner;
+
+public class CountNumberDigits {
+    public static void main(String[] args) {
+	
+	    //create object of scanner class to take input		
+        Scanner s = new Scanner(System.in);
+
+        //taking input from user to enter an integer
+        System.out.print("Enter an integer : ");
+        int number = s.nextInt();
+
+        // Handle negative numbers by converting them to positive using Math.abs
+        number = Math.abs(number);
+
+        int count = 0;
+
+        // Special case : If the number is 0, it has exactly one digit
+        if (number == 0) {
+            count = 1;
+        } 
+		else {
+            //Use a while loop to count the digits
+            while (number != 0) {
+			
+			    // Remove the last digit by dividing the number by 10
+                number = number / 10;  
+				
+                count++;    // Increment the digit count
+            }
+        }
+
+        // Display the total number of digits in the input
+        System.out.println("The number of digits is : " + count);
+
+        s.close(); 
+    }
+}
