dd7d8effbb12b026dd73ac25f28497d8ac292007
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-01-14 01:13:45 +0530

    Replace method done

---
+++ Day05-JavaString/Level 2 Practice Problem/ReplaceMethod.java
@@ -0,0 +1,7 @@
+public class ReplaceMethod{
+	public static void main(String [] args){
+		String str = "We are in capgemini";
+		String result = str.replace('e' , 'o');
+		System.out.println("String after using replace method : " + result);
+	}
+}
